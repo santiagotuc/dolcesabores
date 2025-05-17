64fc925d3c672d6b9e609e3e83f7e954af39165d
Santiago Emmanuel Molina <santiagoemmanuelmolina@gmail.com>
2025-05-17 00:08:22 -0300

    feat: Implementación de la barra de navegación básica

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,32 @@
-import react from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-  return <div> {/*contenido*/} </div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-logo">
+          {/* Aquí irá tu logo cuando lo tengas */}
+          <span>Dulce Sabores</span>
+        </Link>
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">INICIO</Link>
+          </li>
+          <li>
+            <Link to="/categorias">CATEGORIAS</Link>
+          </li>
+          <li>
+            <Link to="/contacto">CONTACTO</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <Link to="/login">LOGIN</Link>
+      </div>
+    </nav>
+  );
 }
+
 export default Navbar;
